c2e85c6e0b848720cc433d05a4d542d58baba3bc
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-09 15:41:34 -0300

    Organizar regex

--- src/silabas.ts
+++ src/silabas.ts
@@ -7,8 +7,8 @@
 function dividirHiatos(palavra: string, aposTonica: boolean = false): string[] {
   const hiatoIndice = palavra.match(
     aposTonica
-      ? /aa|aá|aã|ae|aé|aê|aí|ao|aó|aú|eá|eã|ee|eé|eí|eó|eú|iá|iã|ié|ii|ií|ió|iú|oá|oã|oe|oé|oí|oo|oó|uá|ué|uí|uu|uó|uú/i
-      : /aa|aá|aã|ae|aé|aê|aí|ao|aó|aú|ea|eá|eã|ee|eé|eí|eo|eó|eú|ia|iá|iã|ie|ié|ii|ií|io|ió|iú|oa|oá|oã|oe|oé|oí|oo|oó|(?<!g|q)ua|uá|(?<!g|q)ue|ué|uí|(?<!g|q)uo|uó|uu|uú/i
+      ? /a(a|á|ã|e|é|ê|í|o|ó|ú)|e(á|ã|e|é|í|ó|ú)|i(á|ã|é|i|í|ó|ú)|o(á|ã|e|é|í|o|ó)|u(á|é|í|u|ó|ú)/i
+      : /a(á|ã|e|é|ê|í|o|ó|ú)|e(a|á|ã|e|é|í|o|ó|ú)|i(a|á|ã|e|é|i|í|o|ó|ú)|o(a|á|ã|e|é|í|o|ó)|uá|ué|uí|uó|uu|uú|(?<!g|q)u(a|e|o)/i
   )?.index;
 
   if (hiatoIndice != null) {
