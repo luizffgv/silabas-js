9c2895032cc8cfdc84beec7f18093d59fd370b93
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-09 15:04:55 -0300

    Melhorar detecção de hiatos
    Reduzir falsos positivos em ocorrências de GUA, QUA e GUE

--- src/silabas.ts
+++ src/silabas.ts
@@ -8,7 +8,7 @@ function dividirHiatos(palavra: string, aposTonica: boolean = false): string[] {
   const hiatoIndice = palavra.match(
     aposTonica
       ? /aa|aá|aã|ae|aé|aê|aí|ao|eã|ee|eé|eí|iã|ié|ii|ií|oã|oe|oé|oí|oo|oó|uí|uu/i
-      : /aa|aá|aã|ae|aé|aê|aí|ao|ea|eã|ee|eé|eí|eo|ia|iã|ie|ié|ii|ií|io|oa|oã|oe|oé|oí|oo|oó|ua|(?<!q)ue|uí|uu/i
+      : /aa|aá|aã|ae|aé|aê|aí|ao|ea|eã|ee|eé|eí|eo|ia|iã|ie|ié|ii|ií|io|oa|oã|oe|oé|oí|oo|oó|(?<!g|q)ua|(?<!g|q)ue|uí|uu/i
   )?.index;
 
   if (hiatoIndice != null) {
